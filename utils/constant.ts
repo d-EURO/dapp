4d355a094bbe08400374a505429fa6b580779887
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2024-12-18 16:19:32 +0100

    Update constant.ts

--- utils/constant.ts
+++ utils/constant.ts
@@ -23,5 +23,6 @@ export const MARKETING_PARAM_NAME = "ref";
 export const DEFAULT_FRONTEND_CODE = "0xc155a9c8a3ce42a8268fb22f801479e378d5e70dbcc83db8604b296c6d1d3e10";
 
 export const FRONTEND_CODES: { [key: string]: `0x${string}` } = {
-	dEuro: '0xc155a9c8a3ce42a8268fb22f801479e378d5e70dbcc83db8604b296c6d1d3e10',
+	dEuro  : '0xc155a9c8a3ce42a8268fb22f801479e378d5e70dbcc83db8604b296c6d1d3e10',
+	Michael: '0xdc09f425cfb64bbb93a07bf22ea7e7922e24874971b6d17ce1f91750ad92ef62',
 }
