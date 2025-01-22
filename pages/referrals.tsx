3bad099fa9e44e40f9e135580cca1a3624b05f81
Daniel Padrino <danswarrior1@gmail.com>
2025-01-22 16:34:11 -0300

    [NO-TASK]: Bonus History Table

--- pages/referrals.tsx
+++ pages/referrals.tsx
@@ -3,6 +3,7 @@ import { StartEarningSection } from "@components/PageReferrals/StartEarningSecti
 import { ReferralCenterSection } from "@components/PageReferrals/ReferralCenterSection";
 import { FAQ } from "@components/PageReferrals/FAQ";
 import YourReferralsTable from "@components/PageReferrals/YourReferralsTable";
+import BonusHistoryTable from "@components/PageReferrals/BonusHistoryTable";
 
 export default function Referrals() {
 	return (
@@ -15,6 +16,7 @@ export default function Referrals() {
 				<StartEarningSection />
 				<ReferralCenterSection />
 				<YourReferralsTable data={Array(10).fill({ volume: "256.12", date: "22 Jan 2025", address: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" })} />
+				<BonusHistoryTable data={Array(10).fill({ payout: "256.12", source:"Minting", date: "22 Jan 2025", txId: "0x880d1ab3ee32e6a721312677abdada4bf9e0e40961e16af297932e12dadae3c5" })} />
 				<FAQ />
 			</div>
 		</main>
