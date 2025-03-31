c6605c4a2d2b2ba7c20de26e8f7fbb75c2b9a223
Daniel Padrino <danswarrior1@gmail.com>
2025-03-31 13:30:47 -0300

    minor amends

--- components/FrontendCodeProvider.tsx
+++ components/FrontendCodeProvider.tsx
@@ -1,5 +1,11 @@
 import React, { createContext, useEffect, useState } from "react";
-import { DEFAULT_FRONTEND_CODE, getFrontendCodeFromReferralName, MARKETING_PARAM_NAME, ZERO_FRONTEND_CODE } from "@utils";
+import {
+	DEFAULT_FRONTEND_CODE,
+	getFrontendCodeFromReferralName,
+	getReferralNameFromFrontendCode,
+	MARKETING_PARAM_NAME,
+	ZERO_FRONTEND_CODE,
+} from "@utils";
 import { useRouter } from "next/router";
 import { FrontendGatewayABI } from "@deuro/eurocoin";
 import { zeroAddress } from "viem";
@@ -9,71 +15,72 @@ import { WAGMI_CONFIG } from "../app.config";
 import { useAccount, useChainId } from "wagmi";
 
 interface FrontendCodeContextType {
-    marketingCode: string;
-    frontendCode: `0x${string}`;
+	marketingCode: string;
+	frontendCode: `0x${string}`;
 }
 
 export const FrontendCodeContext = createContext<FrontendCodeContextType | undefined>(undefined);
 
 export const FrontendCodeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [marketingCode, setMarketingCode] = useState("");
-    const [frontendCode, setFrontendCode] = useState<`0x${string}`>(ZERO_FRONTEND_CODE);
-    const router = useRouter();
-    const chainId = useChainId();
-    const { address } = useAccount();
+	const [marketingCode, setMarketingCode] = useState("");
+	const [frontendCode, setFrontendCode] = useState<`0x${string}`>(ZERO_FRONTEND_CODE);
+	const router = useRouter();
+	const chainId = useChainId();
+	const { address } = useAccount();
 
-    useEffect(() => {
-        const fetchReferralCode = async () => {
-            if (!router.isReady) return;
-            const marketingParam = router.query[MARKETING_PARAM_NAME] as string | undefined;
+	useEffect(() => {
+		const fetchReferralCode = async () => {
+			if (!router.isReady) return;
+			const marketingParam = router.query[MARKETING_PARAM_NAME] as string | undefined;
 
-            if (marketingParam) {
-                try {
-                    const code = getFrontendCodeFromReferralName(marketingParam);
-                    const [, owner] = await readContract(WAGMI_CONFIG, {
-                        address: ADDRESS[chainId].frontendGateway,
-                        abi: FrontendGatewayABI,
-                        functionName: "frontendCodes",
-                        args: [code],
-                    });
+			if (marketingParam) {
+				try {
+					const code = getFrontendCodeFromReferralName(marketingParam);
+					const [, owner] = await readContract(WAGMI_CONFIG, {
+						address: ADDRESS[chainId].frontendGateway,
+						abi: FrontendGatewayABI,
+						functionName: "frontendCodes",
+						args: [code],
+					});
 
-                    const hasOwner = owner !== zeroAddress;
-                    if (hasOwner) {
-                        setMarketingCode(marketingParam);
-                        setFrontendCode(code);
-                        return;
-                    }
-                } catch (error) {
-                    console.error("Error checking marketing parameter:", error);
-                }
-            }
-            setMarketingCode("");
-            
-            if (address) {
-                try {
-                    const lastUsedCode = await readContract(WAGMI_CONFIG, {
-                        address: ADDRESS[chainId].frontendGateway,
-                        abi: FrontendGatewayABI,
-                        functionName: "lastUsedFrontendCode",
-                        args: [address],
-                    });
+					const hasOwner = owner !== zeroAddress;
+					if (hasOwner) {
+						setMarketingCode(marketingParam);
+						setFrontendCode(code);
+						return;
+					}
+				} catch (error) {
+					console.error("Error checking marketing parameter:", error);
+				}
+			} else if (address) {
+				try {
+					const lastUsedCode = await readContract(WAGMI_CONFIG, {
+						address: ADDRESS[chainId].frontendGateway,
+						abi: FrontendGatewayABI,
+						functionName: "lastUsedFrontendCode",
+						args: [address],
+					});
 
-                    if (lastUsedCode === ZERO_FRONTEND_CODE) {
-                        setFrontendCode(DEFAULT_FRONTEND_CODE);
-                    } else {
-                        setFrontendCode(ZERO_FRONTEND_CODE);
-                    }
-                } catch (error) {
-                    console.error("Error checking lastUsedFrontendCode:", error);
-                    setFrontendCode(ZERO_FRONTEND_CODE);
-                }
-            } else {
-                setFrontendCode(DEFAULT_FRONTEND_CODE);
-            }
-        };
+					if (lastUsedCode === ZERO_FRONTEND_CODE) {
+						setFrontendCode(DEFAULT_FRONTEND_CODE);
+						setMarketingCode("");
+						return;
+					} else {
+						setFrontendCode(lastUsedCode);
+						setMarketingCode(getReferralNameFromFrontendCode(lastUsedCode));
+						return;
+					}
+				} catch (error) {
+					console.error("Error checking lastUsedFrontendCode:", error);
+				}
+			} else {
+				setFrontendCode(DEFAULT_FRONTEND_CODE);
+				setMarketingCode("");
+			}
+		};
 
-        fetchReferralCode();
-    }, [router.isReady, router.query, chainId, address]);
+		fetchReferralCode();
+	}, [router.isReady, router.query, chainId, address]);
 
-    return <FrontendCodeContext.Provider value={{ marketingCode, frontendCode }}>{children}</FrontendCodeContext.Provider>;
+	return <FrontendCodeContext.Provider value={{ marketingCode, frontendCode }}>{children}</FrontendCodeContext.Provider>;
 };
