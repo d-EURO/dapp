95200c4eabb5391a734a740bb69f97ca4104fb76
David May <david.leo.may@gmail.com>
2026-01-27 16:12:17 +0100

    chore: improved chain config

--- config/chains.ts
+++ config/chains.ts
@@ -1,25 +1,25 @@
 import { defineChain } from "viem";
 
-export const testnet = defineChain({
-	id: 5115,
-	name: "Testnet",
-	nativeCurrency: { name: "cBTC", symbol: "cBTC", decimals: 18 },
-	rpcUrls: {
-		default: { http: ["https://rpc.testnet.citreascan.com"] },
-	},
-	blockExplorers: {
-		default: { name: "CitreaScan", url: "https://testnet.citreascan.com" },
-	},
-});
-
 export const mainnet = defineChain({
 	id: 4114,
-	name: "Mainnet",
+	name: "Citrea",
 	nativeCurrency: { name: "cBTC", symbol: "cBTC", decimals: 18 },
 	rpcUrls: {
 		default: { http: ["https://rpc.citreascan.com"] },
 	},
 	blockExplorers: {
 		default: { name: "CitreaScan", url: "https://citreascan.com" },
+	},
+});
+
+export const testnet = defineChain({
+	id: 5115,
+	name: "Citrea Testnet",
+	nativeCurrency: { name: "cBTC", symbol: "cBTC", decimals: 18 },
+	rpcUrls: {
+		default: { http: ["https://rpc.testnet.citreascan.com"] },
+	},
+	blockExplorers: {
+		default: { name: "CitreaScan", url: "https://testnet.citreascan.com" },
 	},
 });
