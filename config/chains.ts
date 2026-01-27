a62cb04d73a7e1d9d062b34aa044652ef2e46e1f
David May <david.leo.may@gmail.com>
2026-01-27 16:07:24 +0100

    fix: format

--- config/chains.ts
+++ config/chains.ts
@@ -2,24 +2,24 @@ import { defineChain } from "viem";
 
 export const testnet = defineChain({
 	id: 5115,
-	name: 'Testnet',
-	nativeCurrency: { name: 'cBTC', symbol: 'cBTC', decimals: 18 },
+	name: "Testnet",
+	nativeCurrency: { name: "cBTC", symbol: "cBTC", decimals: 18 },
 	rpcUrls: {
-		default: { http: ['https://rpc.testnet.citreascan.com'] },
+		default: { http: ["https://rpc.testnet.citreascan.com"] },
 	},
 	blockExplorers: {
-		default: { name: 'CitreaScan', url: 'https://testnet.citreascan.com' },
+		default: { name: "CitreaScan", url: "https://testnet.citreascan.com" },
 	},
 });
 
 export const mainnet = defineChain({
 	id: 4114,
-	name: 'Mainnet',
-	nativeCurrency: { name: 'cBTC', symbol: 'cBTC', decimals: 18 },
+	name: "Mainnet",
+	nativeCurrency: { name: "cBTC", symbol: "cBTC", decimals: 18 },
 	rpcUrls: {
-		default: { http: ['https://rpc.citreascan.com'] },
+		default: { http: ["https://rpc.citreascan.com"] },
 	},
 	blockExplorers: {
-		default: { name: 'CitreaScan', url: 'https://citreascan.com' },
+		default: { name: "CitreaScan", url: "https://citreascan.com" },
 	},
 });
